63cf405eb954b340dc54bedb4f3045e1540ad151
frankmilito <frankobidike20@gmail.com>
2024-12-11 09:23:07 +0100

    update users route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UserList from "./pages/UserList";
 import UserPosts from "./pages/UserPosts";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<UserList />} />
+      <Route path="/users" element={<UserList />} />
       <Route path="/users/posts/:id" element={<UserPosts />} />
+      <Route path="*" element={<Navigate to="/users" replace />} />
     </Routes>
   );
 }
